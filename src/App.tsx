62ed6d3b3b8e8290ee9cdb24a02d68554811dc8e
Ihor Tkachenko <Igor.tt6@gmail.com>
2023-12-21 13:03:46 +0200

    decompos

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import { useEffect, useState } from "react";
 import Card, { CardVariant } from "./components/Card";
-import UserList from "./components/UserList";
-import { ITodo, IUser } from "./types/types";
-import axios from "axios";
-import List from "./components/List";
-import UserItem from "./components/UserItem";
-import TodoItem from "./components/TodoItem";
 import EventExample from "./components/EventExample";
+import TodosPage from "./components/TodosPage";
+import UserPage from "./components/UserPage";
 
 function App() {
 
-  const [users, setUsers] = useState<IUser[]>([]);
-  const [todos, setTodos] = useState<ITodo[]>([]);
-
-  useEffect(() => {
-    fetchUsers()
-    fetchTodos()
-  }, []);
-
-  async function fetchUsers() {
-    try {
-      const response = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users");
-      setUsers(response.data);
-    } catch (error) {
-      alert(error)
-    }
-  };
-
-  async function fetchTodos() {
-    try {
-      const response = await axios.get<ITodo[]>("https://jsonplaceholder.typicode.com/todos?_limit=10");
-      setTodos(response.data);
-    } catch (error) {
-      alert(error)
-    }
-  }
 
   return (
     <div>
@@ -42,13 +12,8 @@ function App() {
       <Card variant={CardVariant.primary} height="200px" width="200px">
         <button>asdas</button>
       </Card>
-      <List
-        items={users}
-        renderItems={(user: IUser) => <UserItem user={user} key={user.id} />} />
-
-      <List
-        items={todos}
-        renderItems={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />} />
+      <UserPage />
+      <TodosPage />
 
     </div>
   );
