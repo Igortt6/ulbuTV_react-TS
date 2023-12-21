c8b46180f83d392fe3967aff2cb32fd49aa7fe60
Ihor Tkachenko <Igor.tt6@gmail.com>
2023-12-21 14:49:55 +0200

    react Router Dome

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Card, { CardVariant } from "./components/Card";
 import EventExample from "./components/EventExample";
 import TodosPage from "./components/TodosPage";
 import UserPage from "./components/UserPage";
+import { FC } from "react";
 
-function App() {
+const App: FC = () => {
 
 
   return (
-    <div>
-      <EventExample />
-      <Card variant={CardVariant.primary} height="200px" width="200px">
-        <button>asdas</button>
-      </Card>
-      <UserPage />
-      <TodosPage />
+    // <div>
+    //   <EventExample />
+    //   <Card variant={CardVariant.primary} height="200px" width="200px">
+    //     <button>asdas</button>
+    //   </Card>
+    // </div>
+    <Router  >
+      <Link to="/users" >Users</Link>
+      <Link style={{ marginLeft: 20 }} to="/todos">Todos</Link>
 
-    </div>
+      <Routes  >
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/todos" element={<TodosPage />} />
+
+      </Routes>
+    </Router>
+
   );
 }
 
